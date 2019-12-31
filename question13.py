5012fd2a9a7ce599f13c45f4268231ee54b07f24
tUNg131 <halethuy.hlt@gmail.com>
2019-12-31 18:46:31 +0700

    Code in another way

--- question13.py
+++ question13.py
@@ -1,13 +1,28 @@
+# s = input()
+#
+# numbers = 0
+# letters = 0
+#
+# for char in s:
+#     if char.isalpha():
+#         letters += 1
+#     elif char.isnumeric():
+#         numbers += 1
+#
+# print(f"LETTERS {letters}")
+# print(f"NUMBERS {numbers}")
+
 s = input()
 
-numbers = 0
-letters = 0
+d = {"Numbers":0, "Letters":0}
 
 for char in s:
     if char.isalpha():
-        letters += 1
-    elif char.isnumeric():
-        numbers += 1
+        d["Letters"] += 1
+    elif char.isdigit():
+        d["Numbers"] += 1
+    else:
+        pass
 
-print(f"LETTERS {letters}")
-print(f"NUMBERS {numbers}")
+print("LETTERS",d["Letters"])
+print("NUMBERS",d["Numbers"])
